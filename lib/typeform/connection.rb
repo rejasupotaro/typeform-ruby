c0ecaa4481ae12175e3d41b041eb8af8137d2294
Kentaro Takiguchi <kentaro-takiguchi@cookpad.com>
2015-08-31 15:48:47 +0900

    Refactored

--- lib/typeform/connection.rb
+++ lib/typeform/connection.rb
@@ -1,34 +1,34 @@
 module Typeform
   class Connection
-    @@base_uri = "https://api.typeform.io"
+    BASE_URI = "https://api.typeform.io"
+    TYPEFORM_API_VERSION = "v0.3"
 
     def initialize(api_key)
       @api_key = api_key
-      @conn = Faraday.new(url: @@base_uri) do |faraday|
+      @conn = Faraday.new(url: BASE_URI) do |faraday|
         faraday.request :json
         faraday.response :logger
         faraday.adapter  Faraday.default_adapter
-        faraday.use FaradayMiddleware::ParseJson, :content_type => /\bjson$/
+        faraday.use FaradayMiddleware::ParseJson, content_type: /\bjson$/
       end
     end
 
     def set_base_headers(request)
       request.headers["X-API-TOKEN"] = @api_key
-      request.headers["Content-Type"] = "application/json; charset=utf-8"
       request.headers["Accept-Encoding"] = "gzip, deflate"
     end
 
     def get(path)
       @conn.get do |request|
         set_base_headers(request)
-        request.url "/v0.3#{path}"
+        request.url "/#{TYPEFORM_API_VERSION}#{path}"
       end
     end
 
     def post(path, body)
       @conn.post do |request|
         set_base_headers(request)
-        request.url "/v0.3#{path}"
+        request.url "/#{TYPEFORM_API_VERSION}#{path}"
         request.body = body
       end
     end
--- typeform.gemspec
+++ typeform.gemspec
@@ -27,6 +27,5 @@ Gem::Specification.new do |spec|
 
   spec.add_dependency "faraday"
   spec.add_dependency "faraday_middleware"
-  spec.add_dependency "json"
   spec.add_dependency "json-encodable"
 end
